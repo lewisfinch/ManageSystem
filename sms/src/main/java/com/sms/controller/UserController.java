29a69d1996496037dbcc96579b46ed393c333dd2
lewisFinch <defalt404@163.com>
2024-09-08 14:33:07 -0400

    CRUD methods implemented

--- sms/src/main/java/com/sms/controller/UserController.java
+++ sms/src/main/java/com/sms/controller/UserController.java
@@ -1,13 +1,11 @@
 package com.sms.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.sms.entity.User;
 import com.sms.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,4 +29,31 @@ public class UserController {
         return userService.list();
     }
 
+    //新增
+    @PostMapping("/save")
+    public boolean save(@RequestBody User user){
+        return userService.save(user);
+    }
+    //修改
+    @PostMapping("/mod")
+    public boolean mod(@RequestBody User user){
+        return userService.updateById(user);
+    }
+    //新增或修改
+    @PostMapping("/saveOrMod")
+    public boolean saveOrMod(@RequestBody User user){
+        return userService.saveOrUpdate(user);
+    }
+    //删除
+    @GetMapping("/delete")
+    public boolean delete(Integer id){
+        return userService.removeById(id);
+    }
+    //查询
+    @PostMapping("/listP")
+    public List<User> listP(@RequestBody User user){
+        LambdaQueryWrapper<User> lambdaQueryWrapper = new LambdaQueryWrapper();
+        lambdaQueryWrapper.eq(User::getName, user.getName());
+        return userService.list(lambdaQueryWrapper);
+    }
 }
